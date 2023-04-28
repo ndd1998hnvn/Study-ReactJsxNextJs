37ed9af611d4faaf029288e29078afa4212a2778
ndd1998hnvn <64448156+ndd1998hnvn@users.noreply.github.com>
2023-04-28 10:24:11 +0700

    test deploy

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome learn ReactJs by Cop</h1>
+      <h1 style={{color:'red' }}> Chào mừng bạn đến với khóa học ReactJs với Nextjs cùng với Cop</h1>
       <ul>
         <li><Link href='/'>HomePage</Link></li>
         <li><Link href='/about'>About</Link></li>
         <li><Link href='/about/team'>Team</Link></li>
-      </ul>
+      </ul>      
     </div>
   )    
 }
